bfaa557f44253d527c828c4e688eeaeacf0232c4
Sarra Mzoughi <sarramzoughi765@gmail.com>
2026-02-11 23:35:47 +0100

    Week 2: First React component with JSX

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+/*1. One thing I understand well: How to use curly braces {} to display 
+     JavaScript variables and objects inside HTML tags.
+  2. One thing that is still confusing: Why React requires us to use 
+     specific names like 'htmlFor' instead of 'for'.
+  3. One mistake I made and fixed: I forgot to export the App at first, 
+     which caused the screen to go blank.const courseTitle = "React dev Lab";*/
+ function App() {
+  const studentName = "Sarra";
+  const student= {
+    name : "sarra",
+    age: 21,
+    track :"Fullstack JavaScript"
+  };
+  const sayHello= () => {return `hello ${studentName}, welcome to the react world`:};
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+      <h1>This is my first React project</h1>
+      <h2>my name is : {studentName}</h2>
+      <h3>course : {courseTitle}</h3>
+      <p> welcome to {courseTitle}, {studentName} !</p>
+      <label htmlFor= "feedback-input">enter your feedback:</label>
+      <input id="feedback-input" type= "text"/>
+      <p> name: {student.name}- Age: {student.age}- Track: {student.track}</p>
+      <p>function says: {sayHello()}</p>
 
-export default App
+
+    </div>
+  );} export default App;
